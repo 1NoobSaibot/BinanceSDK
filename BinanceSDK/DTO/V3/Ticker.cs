bf2f1ba02eebdc60efe03f9dfbe55446334a1708
Evhen Diulin <zheka.diulin@gmail.com>
2023-02-09 21:03:32 +0200

    Init

---
+++ BinanceSDK/DTO/V3/Ticker.cs
@@ -0,0 +1,79 @@
+﻿using Newtonsoft.Json;
+
+namespace BinanceSDK.DTO.V3
+{
+	public class Ticker
+	{
+		[JsonProperty("symbol")]
+		public string Symbol;
+
+		[JsonProperty("priceChange")]
+		public decimal PriceChange;
+
+		[JsonProperty("priceChangePercent")]
+		public decimal PriceChangePercent;
+
+		[JsonProperty("weightedAvgPrice")]
+		public decimal WeightedAvgPrice;
+
+		[JsonProperty("prevClosePrice")]
+		public decimal PrevClosePrice;
+
+		[JsonProperty("lastPrice")]
+		public decimal LastPrice;
+
+		/// <summary>
+		/// TODO: Check the value
+		/// </summary>
+		[JsonProperty("lastQty")]
+		public decimal LastPriceQuote;
+
+		[JsonProperty("bidPrice")]
+		public decimal BidPrice;
+
+		/// <summary>
+		/// TODO: Check the value
+		/// </summary>
+		[JsonProperty("bidQty")]
+		public decimal BidQuote;
+
+		[JsonProperty("askPrice")]
+		public decimal AskPrice;
+
+		/// <summary>
+		/// TODO: Check the value
+		/// </summary>
+		[JsonProperty("askQty")]
+		public decimal AskPriceQuote;
+
+		[JsonProperty("openPrice")]
+		public decimal OpenPrice;
+
+		[JsonProperty("highPrice")]
+		public decimal HighPrice;
+
+		[JsonProperty("lowPrice")]
+		public decimal LowPrice;
+
+		[JsonProperty("volume")]
+		public decimal Volume;
+
+		[JsonProperty("quoteVolume")]
+		public decimal VolumeQuote;
+
+		[JsonProperty("openTime")]
+		public long OpenTime;
+
+		[JsonProperty("closeTime")]
+		public long CloseTime;
+
+		[JsonProperty("firstId")]
+		public long FirstTradeId;
+
+		[JsonProperty("lastId")]
+		public long LastTraadeId;
+
+		[JsonProperty("count")]
+		public int TradeCount;
+	}
+}
---
+++ BinanceSdkTest/PublicRestTest.cs
@@ -0,0 +1,17 @@
+using BinanceSDK;
+
+namespace BinanceSdkTest
+{
+	[TestClass]
+	public class PublicRestTest
+	{
+		[TestMethod]
+		public void ShouldGetTickers()
+		{
+			PublicRest client = new PublicRest();
+			var tickers = client.GetTickers();
+			Assert.IsNotNull(tickers);
+			Assert.IsTrue(tickers.Length > 0);
+		}
+	}
+}
