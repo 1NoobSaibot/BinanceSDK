c5c1814f155a1dd4999316b5e1c7d112a0c1ff1b
Evhen Diulin <zheka.diulin@gmail.com>
2023-02-10 17:14:36 +0200

    Two filters which I don't really need now:)

---
+++ BinanceSDK/Filters/MinNotionalFilter.cs
@@ -0,0 +1,22 @@
+﻿namespace BinanceSDK.Filters
+{
+	/// <summary>
+	/// The MIN_NOTIONAL filter defines the minimum notional value allowed for an order on a symbol.
+	/// An order's notional value is the price * quantity.
+	/// If the order is an Algo order (e.g. STOP_LOSS_LIMIT),
+	/// then the notional value of the stopPrice * quantity will also be evaluated.
+	/// If the order is an Iceberg Order,
+	/// then the notional value of the price * icebergQty will also be evaluated.
+	/// applyToMarket determines whether or not the MIN_NOTIONAL filter will also be applied to MARKET orders.
+	/// Since MARKET orders have no price, the average price is used over the last avgPriceMins minutes.
+	/// avgPriceMins is the number of minutes the average price is calculated over.
+	/// 0 means the last price is used.
+	/// </summary>
+	/// TODO: Find out how to implement this filter
+	public class MinNotionalFilter
+	{
+		public readonly decimal MinNotional;
+		public readonly bool ApplyToMarket;
+		public readonly decimal AveragePriceForLastMinutes;
+	}
+}
---
+++ BinanceSDK/Filters/NotionalFilter.cs
@@ -0,0 +1,29 @@
+﻿namespace BinanceSDK.Filters
+{
+	/*
+		The NOTIONAL filter defines the acceptable notional range allowed for an order on a symbol.
+
+		applyMinToMarket determines whether the minNotional will be applied to MARKET orders.
+
+		applyMaxToMarket determines whether the maxNotional will be applied to MARKET orders.
+
+		In order to pass this filter, the notional (price * quantity) has to pass the following conditions:
+
+		price * quantity <= maxNotional
+		price * quantity >= minNotional
+		For MARKET orders, the average price used over the last avgPriceMins minutes will be used for calculation.
+
+		If the avgPriceMins is 0, then the last price will be used.
+	 */
+	public class NotionalFilter
+	{
+		public readonly decimal MinNotional;
+		public readonly bool ApplyMinToMarket;
+		public readonly decimal MaxNotional;
+		public readonly bool ApplyMaxToMarket;
+		/// <summary>
+		/// "avgPriceMins": 5
+		/// </summary>
+		public readonly int ForLastMinutes;
+	}
+}
