f4024bbb407b6a4d14e3bf4c6becb01d1b4c7707
Evhen Diulin <zheka.diulin@gmail.com>
2023-02-10 15:09:45 +0200

    Price filter

---
+++ BinanceSDK/Filters/PriceFilter.cs
@@ -0,0 +1,51 @@
+﻿namespace BinanceSDK.Filters
+{
+	public class PriceFilter
+	{
+		/// <summary>
+		/// minPrice defines the minimum price/stopPrice allowed; disabled on minPrice == 0.
+		/// </summary>
+		public readonly decimal MinPrice;
+
+		/// <summary>
+		/// maxPrice defines the maximum price/stopPrice allowed; disabled on maxPrice == 0.
+		/// </summary>
+		public readonly decimal MaxPrice;
+
+		/// <summary>
+		/// tickSize defines the intervals that a price/stopPrice can be increased/decreased by; disabled on tickSize == 0.
+		/// </summary>
+		public readonly decimal TickeSize;
+
+
+		public PriceFilter(decimal minPrice, decimal maxPrice, decimal tickSize)
+		{
+			MinPrice = minPrice;
+			MaxPrice = maxPrice;
+			TickeSize = tickSize;
+		}
+
+
+		public bool IsPriceValid(decimal price)
+		{
+			if (price < MinPrice)
+			{
+				return false;
+			}
+
+			if (MaxPrice > 0 && price > MaxPrice)
+			{
+				return false;
+			}
+
+			// price mod tickSize MUST BE 0
+			if (TickeSize == 0)
+			{
+				return true;
+			}
+
+			decimal res = price / TickeSize;
+			return (res - ((int)res)) == 0m;
+		}
+	}
+}
---
+++ BinanceSdkTest/Filters/PriceFilterTest.cs
@@ -0,0 +1,97 @@
+﻿using BinanceSDK.Filters;
+
+namespace BinanceSdkTest.Filters
+{
+	[TestClass]
+	public class PriceFilterTest
+	{
+		[TestMethod]
+		public void ShouldFilterCorrect()
+		{
+			foreach (var pair in _examples)
+			{
+				var filter = pair.Key;
+				var samples = pair.Value;
+				foreach (var sample in samples)
+				{
+					Assert.AreEqual(sample.isValid, filter.IsPriceValid(sample.price));
+				}
+			}
+		}
+
+
+		private Dictionary<PriceFilter, List<(decimal price, bool isValid)>> _examples
+			= new Dictionary<PriceFilter, List<(decimal price, bool isValid)>>()
+			{
+				{ 
+					new PriceFilter(0, 0, 0),
+					new List<(decimal price, bool isValid)>()
+					{
+						(0, true),
+						(1000, true),
+						(-1, false),
+						(0.0000000001m, true)
+					}
+				},
+				{
+					new PriceFilter(5, 0, 0),
+					new List<(decimal price, bool isValid)>()
+					{
+						(0, false),
+						(-1, false),
+						(4, false),
+						(4.9999999999999m, false),
+						(5, true),
+						(6, true),
+						(1000, true),
+						(7.0000000001m, true)
+					}
+				},
+				{
+					new PriceFilter(0, 100, 0),
+					new List<(decimal price, bool isValid)>()
+					{
+						(0, true),
+						(-1, false),
+						(4, true),
+						(4.9999999999999m, true),
+						(5, true),
+						(1000, false),
+						(0.0000000001m, true)
+					}
+				},
+				{
+					new PriceFilter(5, 100, 0),
+					new List<(decimal price, bool isValid)>()
+					{
+						(0, false),
+						(-1, false),
+						(4, false),
+						(4.9999999999999m, false),
+						(5, true),
+						(6, true),
+						(100, true),
+						(100.00000001m, false),
+						(1000, false),
+						(8.0000000001m, true)
+					}
+				},
+				{
+					new PriceFilter(0, 0, 0.01m),
+					new List<(decimal price, bool isValid)>()
+					{
+						(0, true),
+						(-1, false),
+						(4, true),
+						(4.9999999999999m, false),
+						(5.99m, true),
+						(6.01m, true),
+						(100, true),
+						(100.00000001m, false),
+						(1000.03m, true),
+						(8.0000000001m, false)
+					}
+				},
+			};
+	}
+}
